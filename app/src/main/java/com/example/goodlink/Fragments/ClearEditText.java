abf37feb51dda9b40e9d86f25a938d2b99c98d04
Igor Eduardo <118550502+IgorEdu-Silva@users.noreply.github.com>
2024-04-06 16:46:52 -0300

    adicionei novas funções

---
+++ app/src/main/java/com/example/goodlink/Fragments/ClearEditText.java
@@ -0,0 +1,22 @@
+package com.example.goodlink.Fragments;
+
+
+import androidx.fragment.app.Fragment;
+
+
+public abstract class ClearEditText extends Fragment {
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
