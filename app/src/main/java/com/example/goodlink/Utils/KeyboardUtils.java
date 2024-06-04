5aa7cf278aa56424a90c90de917b859b713a7889
Igor Eduardo <118550502+IgorEdu-Silva@users.noreply.github.com>
2024-06-04 19:50:35 -0300

    adicionei funcionabilidades

--- app/src/main/java/com/example/goodlink/Utils/KeyboardUtils.java
+++ app/src/main/java/com/example/goodlink/Utils/KeyboardUtils.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 
 public class KeyboardUtils {
+
     public interface KeyboardVisibilityListener {
         void onKeyboardVisibilityChanged(boolean isVisible, int keyboardHeight);
     }
