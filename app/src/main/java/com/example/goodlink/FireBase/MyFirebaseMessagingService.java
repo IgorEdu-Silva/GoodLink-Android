93c3ccf093b6bfe6fcb5e99ebeae869e66d484f4
Igor Eduardo <118550502+IgorEdu-Silva@users.noreply.github.com>
2024-06-04 19:48:38 -0300

    adicionei funcionabilidades

--- app/src/main/java/com/example/goodlink/FireBase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/goodlink/FireBase/MyFirebaseMessagingService.java
@@ -15,7 +15,6 @@ import com.example.goodlink.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseMessaging";
     public static final String ACTION_TOKEN_RECEIVED = "com.example.goodlink.ACTION_TOKEN_RECEIVED";
